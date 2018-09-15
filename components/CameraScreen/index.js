021baf44c65ecb76c63ae9bd86b45c812bb77365
Akshit Dewan <akshit.dewan@yahoo.com>
2018-09-15 04:29:39 -0400

    barcode scanner

--- components/CameraScreen/index.js
+++ components/CameraScreen/index.js
@@ -1,58 +1,96 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+// import React from 'react';
+// import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+// import { Camera, Permissions, BarCodeScanner } from 'expo';
 
-class CameraScreen extends React.Component {
+// class CameraScreen extends React.Component {
+//   state = {
+//     hasCameraPermission: null,
+//     type: Camera.Constants.Type.back,
+//   };
+
+//   async componentWillMount() {
+//     const { status } = await Permissions.askAsync(Permissions.CAMERA);
+//     this.setState({ hasCameraPermission: status === 'granted' });
+//   }
+
+//   render() {
+//     const { hasCameraPermission } = this.state;
+
+//     if (hasCameraPermission === null) {
+//       return <Text>Requesting for camera permission</Text>;
+//     }
+//     if (hasCameraPermission === false) {
+//       return <Text>No access to camera</Text>;
+//     }
+//     return (
+//       <View style={{ flex: 1 }}>
+//         <Camera
+//           onBarCodeRead={this._handleBarCodeRead}
+//           style={StyleSheet.absoluteFill}
+//         />
+//       </View>
+//     );
+//   }
+
+//   _handleBarCodeRead = data => {
+//     Alert.alert(
+//       'Scan successful!',
+//       JSON.stringify(data)
+//     );
+//   };
+// }
+
+// export default CameraScreen;
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Alert } from 'react-native';
+import { Constants, Camera, Permissions } from 'expo';
+
+export default class CameraScreen extends Component {
   state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
+    hasCameraPermission: null
   };
 
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+  componentDidMount() {
+    this._requestCameraPermission();
   }
 
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = data => {
+    Alert.alert(
+      'Scan successful!',
+      JSON.stringify(data)
+    );
+  };
+
   render() {
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ?
+          <Text>Requesting for camera permission</Text> :
+          this.state.hasCameraPermission === false ?
+            <Text>Camera permission is not granted</Text> :
+            <Camera
+              onBarCodeRead={this._handleBarCodeRead}
+              style={StyleSheet.absoluteFill}
+            />
+        }
+      </View>
+    );
   }
 }
 
-export default CameraScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  }
+});
